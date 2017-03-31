027b3ec7e5117417361593422c273101c4527ea9
Prav7728 <pravir.raghu@hotmail.com>
2017-03-31 11:43:23 +0200

    added social icons, updated fusion screen
    added social icons, updated fusion screen

--- js/onelife.js
+++ js/onelife.js
@@ -175,3 +175,8 @@ function playVideo() {
 function bringBackPlayButton() {
     $('.playoverlay').css("display", 'block');
 }
+
+
+function reverseColor(){
+    $("#instagramLogo").css("color","#FFF").css("background","-moz-linear-gradient(45deg, #f09433 0%, #e6683c 25%, #dc2743 50%, #cc2366 75%, #bc1888 100%)").css("background","-webkit-linear-gradient(45deg, #f09433 0%, #e6683c 25%, #dc2743 50%, #cc2366 75%, #bc1888 100%);").css("background","linear-gradient(45deg, #f09433 0%, #e6683c 25%, #dc2743 50%, #cc2366 75%, #bc1888 100%);")
+}
